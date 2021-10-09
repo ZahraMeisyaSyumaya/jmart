354b14fb46edc0699c86012380ede1dd718fdef8
ZahraMeisyaSyumaya <zahra.meisya@ui.ac.id>
2021-10-09 10:43:15 +0700

    Zahra - modul 5

--- src/zahraJmartRK/Recognizable.java
+++ src/zahraJmartRK/Recognizable.java
@@ -1,13 +1,6 @@
 package zahraJmartRK;
 
-
-/**
- * Write a description of class Recognizable here.
- *
- * @author (your name)
- * @version (a version number or a date)
- */
-public abstract class Recognizable
+public class Recognizable
 {
     public final int id;
 
